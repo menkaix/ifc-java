5e94aa3da267d05f2e31c0d8082317e34d8e3d64
menka.ix <mendrika.irina@gmail.com>
2019-01-08 18:40:49 +0300

    just the beginning

--- IFCTools/src/ifc/data/IfcProject.java
+++ IFCTools/src/ifc/data/IfcProject.java
@@ -2,11 +2,40 @@ package ifc.data;
 
 import java.util.Vector;
 
+import ifc.io.IfcFileLoader;
+
 public class IfcProject {
 	
 
 	public Vector<IfcObject> ifcData = new Vector<IfcObject>();
-		
+	
+	public Vector<String> types = new Vector<String>();
+	
+	public void loadFile(String path) {
+		try {
+			IfcFileLoader.load(path, this);
+			System.out.println("done");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void displayCountData() {
+		System.out.println(ifcData.size());
+	}
+	
+	public void displayCountTypes() {
+		System.out.println(types.size());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	public void echo() {
 		System.out.println("here !");
 	}
---
+++ IFCTools/src/ifc/io/IfcFileLoader.java
@@ -0,0 +1,53 @@
+package ifc.io;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import ifc.data.IfcObject;
+import ifc.data.IfcProject;
+
+public class IfcFileLoader {
+	
+	public static void load(String fileName, IfcProject project) {
+		
+		try {
+			BufferedReader reader =  new BufferedReader(new FileReader(fileName));
+			
+			String strSource = null ;
+			
+			do {
+				
+				strSource = reader.readLine();;
+				
+				if(strSource!= null && strSource.startsWith("#")) {
+					
+					IfcObject ifcObj = new IfcObject(strSource);
+					
+					project.ifcData.addElement(ifcObj);
+					
+					if(!project.types.contains(ifcObj.IFCName)) {
+						
+						project.types.add(ifcObj.IFCName);
+						
+					}
+					
+				}
+				
+				
+			}while (strSource != null);			
+			
+			reader.close();
+			
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+
+}
